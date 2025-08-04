35856f719f3679adb0cecf5f3a6a7e425e301a75
Trinesh Chintanippu <trineshch7@gmail.com>
2025-08-05 00:04:49 +0530

    🔧 fix: added onrender.com to CORS

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:5173", "https://easy-bike.vercel.app"],
+		origin: ["http://localhost:5173", "https://easy-bike.onrender.com"],
 		credentials: true,
 	})
 );
@@ -30,7 +30,7 @@ const server = http.createServer(app);
 // Set up Socket.IO
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:5173", "https://easy-bike.vercel.app"],
+		origin: ["http://localhost:5173", "https://easy-bike.onrender.com"],
 		methods: ["GET", "POST"],
 		credentials: true,
 	},
